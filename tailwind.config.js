9f933be2d8d98507a01baa4bd338b9b00a74c1db
Diego Torres <datorres@ucompensar.edu.co>
2025-06-26 17:04:21 -0500

    tailwindcss was added

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#F0F0E3',
+        primary: '#53A09D',
+        secondary: '#3D7D7A',
+        accent: '#9ED3D1',
+        neutral: '#E0E0D8',
+        textPrimary: '#333333',
+        textSecondary: '#666666',
+        textOnPrimary: '#FFFFFF',
+      },
+    },
+  },
+  plugins: [],
+}
